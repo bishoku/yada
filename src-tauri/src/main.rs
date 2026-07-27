b5050530807ba116d0d57303c086b3787f94ec2b
bishoku <barishoku@gmail.com>
2026-07-27 21:53:54 +0300

    chore: rename application to Yada and add Linux WebKit DMABUF workaround

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    tauri_app_lib::run()
+    #[cfg(target_os = "linux")]
+    {
+        if std::env::var("WEBKIT_DISABLE_DMABUF_RENDERER").is_err() {
+            std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+        }
+    }
+
+    yada_lib::run()
 }
